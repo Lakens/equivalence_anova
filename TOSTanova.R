cf60578d0c18465b6d7a7c04e294b8b69bb79979
Daniel Lakens <Lakens@gmail.com>
2018-08-04 11:42:47 +0200

    update graph

--- TOSTanova.R
+++ TOSTanova.R
@@ -62,33 +62,36 @@ noninf_Fstat <-  function(Fstat, df1, df2, N, eq_bound_eta, alpha = 0.10, tol = 
     crit_f <- qf(1 - alpha, df1, df2, ncp = 0)
     
     eta_pop_dist <- function(x) df((x*df2)/(df1-x*df1), df1, df2, ncp = ncp)
-    par(bg = "aliceblue")
-    plot(-10,xlab=substitute(paste(eta[p]^2)), ylab="Density", axes=FALSE,
-         main=substitute(paste("distribution for ",eta[p]^2 == eta2_hat,".")), xlim=c(0,xmax),  ylim=c(0, ymax))
+    par(bg = "white")
+    plot(-10,xlab=substitute(paste(eta[p]^2)), ylab="", axes=FALSE,
+         xlim=c(0,xmax),  ylim=c(0, ymax))
     axis(side=1, at=seq(0,xmax, 0.1), labels=seq(0,xmax, 0.1))
-    axis(side=2)
+    #axis(side=2)
+    title(main=paste("Equivalence bound = ",round(eq_bound_eta,digits=3),"\nObserved eta = ",round(eta2_hat,digits=3)," \n TOST ", 100*(1-alpha),"% CI [",round(LL_CI,digits=3),";",round(UL_CI,digits=3),"], p = ",round(pval,digits=3),sep=""), cex.main=1)
+    
     ncp<-0
     eta_pop_crit<-(crit_f*df1)/(crit_f*df1+df2)
-    #Draw null distribution
-    ncp<-0 #So set ncp to null
-    curve(eta_pop_dist, 0.00000000001, 0.99999999, n=10000, col="grey", lwd=2, add=TRUE)
-    x=seq(eta_pop_crit,xmax,length=10000) 
-    z<-df((x*df2)/(df1-x*df1), df1, df2) #determine upperbounds polygon
-    polygon(c(eta_pop_crit,x,xmax),c(0,z,0),col=rgb(1, 0, 0,0.5)) #draw polygon
+    # #Draw null distribution
+    # ncp<-0 #So set ncp to null
+    # curve(eta_pop_dist, 0.00000000001, 0.99999999, n=10000, col="grey", lwd=2, add=TRUE)
+    # x=seq(eta_pop_crit,xmax,length=10000) 
+    # z<-df((x*df2)/(df1-x*df1), df1, df2) #determine upperbounds polygon
+    # polygon(c(eta_pop_crit,x,xmax),c(0,z,0),col=rgb(1, 0, 0,0.5)) #draw polygon
     #Add Type 2 error rate
     ncp <- ncp_hat #So set ncp to observed value
     curve(eta_pop_dist, 0.00000000001, 0.99999999, n=10000, col="black", lwd=2, add=TRUE)
-    y=seq(0.00000000001,eta_pop_crit,length=10000) 
-    z<-df((y*df2)/(df1-y*df1), df1, df2, ncp) #determine upperbounds polygon
-    polygon(c(y,eta_pop_crit,eta_pop_crit),c(0,z,0),col=rgb(0, 0, 1,0.5))
+    # y=seq(0.00000000001,eta_pop_crit,length=10000) 
+    # z<-df((y*df2)/(df1-y*df1), df1, df2, ncp) #determine upperbounds polygon
+    # polygon(c(y,eta_pop_crit,eta_pop_crit),c(0,z,0),col=rgb(0, 0, 1,0.5))
     #segments(eta_pop_crit, 0, eta_pop_crit, ymax-0.03, col= 'black', lwd=2)
     segments(LL_CI, 0, LL_CI, ymax-0.03, col = 'grey', lwd = 2, lty = 2)
     segments(UL_CI, 0, UL_CI, ymax-0.03, col = 'grey', lwd = 2, lty = 2)
     segments(eta2_hat, 0, eta2_hat, ymax-0.03, col = 'darkgrey', lwd = 2, lty = 1)
+    points(x=eta2_hat, y=0.7, pch=15, cex=2)
+    segments(LL_CI,0.7,UL_CI,0.7, lwd=3)
     
     
-    
-    invisible(list(eta2_hat = eta2_hat, TOST_p = pval, alpha = alpha, eq_eqbound_eta = eq_bound_eta, LL_CI_TOST = LL_CI, UL_CI_TOST = UL_CI))
+    invisible(list(eta2_hat = , TOST_p = pval, alpha = alpha, eq_eqbound_eta = eq_bound_eta, LL_CI_TOST = LL_CI, UL_CI_TOST = UL_CI))
   
 }
 
@@ -98,7 +101,7 @@ Fstat <- 6
 df1 <- 1
 df2 <- 48
 N <- 50
-eq_bound_eta <- 0.34
+eq_bound_eta <- 0.38
 
 res <- noninf_Fstat(Fstat = Fstat, df1 = df1, df2 = df2, N = N, eq_bound_eta = eq_bound_eta)
 res
